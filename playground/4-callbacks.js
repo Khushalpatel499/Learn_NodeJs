80cd266398f8bef61a30dcb6686c327d425334ca
khushalpatel499 <khushalpatel499@gmail.com>
2024-04-21 23:09:03 +0530

    add new files

---
+++ playground/4-callbacks.js
@@ -0,0 +1,90 @@
+// a callback function a is nothing more than a function we provide as an argument to another function with the intention of having it called later on.
+setTimeout(() => {
+  console.log("Two seconds are up");
+}, 2000);
+
+//we are using the callback pattern in an asynchronus way, we know that
+// settimeout is an node provided api and it is indeed a synchronous.
+// now that doesnot mean every time we use the callback pattern its actually a synchronous
+//we know that settimeout uses the callback pattern and its asynchronous and we know that our array methods like for each and filter use the callback pattern but they are indeed synchronous.
+
+//goal is to use callback with filter
+// we use the filter function  and we are going to provide a callback function and passing an another function.
+
+const names = ["khushal", "rahul", "mohit"];
+const shortNames = names.filter((name) => {
+  return name.length <= 5;
+});
+
+// in above case we are indeed using the callback pattern as well but there is nothing asynchronous about filter ,
+//it not interacting with a native node API it is just standard javascript  code completly synchronous.
+// here we used the callback pattern is when we are working with the function that we never define, we pass a callback to settimeout this is nodejs api and we passed callback to filter also not deffined by us, that comes from js.
+// imagine in app.js program there are four different places where i want to be able to take a location and get the coordinates back for that i have to copy the geocode 4 times on different position in app.js. thats not ideal.
+//we would create a function called GEO code or something like it and all of these code would go inside of there.
+
+// const geocode = (address, callback) => {
+//   const data = {
+//     latitude: 0,
+//     longitude: 0,
+//   };
+//   return data;
+//   //now the goal is to give it back to the caller of geo code.
+// };
+// //now there are two ways to get this done
+// //1. it could be the return value from the geocode.
+// //2. we provide a callback and get the data there.
+// const data = geocode("Philadelphia");
+// console.log(data);
+//the problem here is that there is nothing happening asynchronous inside .
+//we gonna take all of the code inside of the GEO code function.
+
+const geocode = (address, callback) => {
+  setTimeout(() => {
+    const data = {
+      latitude: 0,
+      longitude: 0,
+    };
+    // return data;
+    callback(data);
+  }, 2000);
+};
+
+// const data = geocode("Philadelphia");
+// console.log(data);
+//now we show that undefined printing
+// means the geocode function not returing anything.
+// there is a single settimeout and no return statment directly inside of geo code.
+//so geocode finishes immediately.
+// if we don't return  something from a function we know that js will implicitly return fuction
+//there is return statement but it is nested inside
+//so the return pattern is no longer going to work for us when we start to do asynchronous thing inside
+//that what the callback pattern is gong to come into play.
+//so the change our code to use calback and get it back to a working state.
+
+geocode("Philadelphia", (data) => {
+  console.log(data);
+});
+// instead of returning of data we are gonna go ahead and call the callback function passing the data in as that first argument.
+//if we are calling the callback with data as the first argument that means we are calling this function(geocode()) with data as the first argumnet.
+
+//so if our function is completely synchronous like geo code was before we are able to use return to get the value out of the function and back to the part of the code that called that functon when our function
+// starts to do something asynchronous though that's no longer option so instead of return a value we take a call back in and we call the callback with the value we want send back when we have it.
+
+//Goal:mess around with the callback pattern
+
+//1. Define an add function that accepts the correct arguments.
+//3. Use settimeout to simulate a 2 second delay.
+//3. After 2 seocnd are up, call the callback function with the sum
+//4.Test your
+
+const add = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+    // return ;
+    // i can't use return here beacuse i am returning from this inner function not from add, instead of using return i will call callback
+  }, 2000);
+};
+add(1, 4, (sum) => {
+  console.log(sum);
+});
+//we seen it used with set time now to simulate the asynchronous process.
--- weather-app/app.js
+++ weather-app/app.js
@@ -54,17 +54,29 @@ const url =
 request({ url: url, json: true }, (error, response) => {
   //   const data = JSON.parse(response.body);
   //   console.log(data.currently);
-  console.log(response.body.currently);
-  console.log(
-    response.body.daily.data[0].summery +
-      "It is currently" +
-      response.body.currently.temperature +
-      "degree out." +
-      "There is a" +
-      response.body.currently.precipProbability +
-      "."
-  );
+  // here the condition if we are not connected to internet then error handling but what when we have bad coordinates then we indeed get a response with two properties code and error and we need to add a little bit of defensive programming to make sure we print a useful error message to the user in this situation.
+  //so when we have invalid input the server might response and it going to respond with data that might be considered an error but it not going to populate this error object. because the error in request is used for lower level of os things lik the complete lack of network connection.
+  if (error) {
+    console.log("Enable to connect the weather services");
+  } else if (response.body.error) {
+    console.log("Enable to find the Location");
+  } else {
+    //request module automatically pass the jason data ,we can do this by customizing the options object of jason:true
+    console.log(response.body.currently);
+    // it much easier to use the browser to explore what data i want to include fromm a json response.
+    //we gonna use the chrome extension json formatter
+    console.log(
+      response.body.daily.data[0].summery +
+        "It is currently" +
+        response.body.currently.temperature +
+        "degree out." +
+        "There is a" +
+        response.body.currently.precipProbability +
+        "."
+    );
+  }
 });
+//we have exclude to leave of the forecase we arenot using to makeour response faster.we can use lang and units. we can set these by key value pair after the url by ? and separeted by &.
 
 //now we learn how request automatically parse jason.
 
@@ -76,8 +88,56 @@ request({ url: url, json: true }, (error, response) => {
 // 1. Print: "It is currently 58.55 degrees out. There is a 0% chance of rain."
 // 2. Test your work!
 
+//Geocoding it is process of taking and address and converting it into a latitude and longitude coordinate pair.
 //use another http api which provide a geo coding serivce,it is process of taking address
 //and converting that into latitude and longitude coordinates
 // and then we gone pass them to darksky api to get weather information
 //in end it required two http request to build out the weather app.
 //we used mapbox.com to sort interesting location based seriveces
+// Address --> lat/long --> weather
+// mapbox provide a secret key that's going to allow it to track how many request we are actually making
+// this api support two geocoding 1.forward and 2. reverse
+// forward geo coding do change address into lang,lati while reverse do opposite.
+//there are two required parameters 1.endpoint(we use mapbox.places this is main one and other is reserved for enterprise customer) and 2.search_text(here we provide address)
+//they are using the curl command from the terminal to fetch the given end points.
+// it start off with the base URL that is a https api mapbx.com then gecoding api with version 5 and then we provide the two required parameters the mapbox.places and then we have search term with .json to get json data back and then start with query string with keyvalue pair that is access token
+// root of our geocoding is an object with four properties type,query,features,and attribution.
+// query just lets us know what we provided like search terms that we provided via the URL.
+// features array contain the interesting data we want to extract
+//by default mapbox is going to return the five most relevant search result and the most relevent one come first .
+// the coordinates are stored in features of center with two numbers first number that is the longitude and the second number latitude. and we extract these two values from the json response and pass to the dark sky api to fetch the eather for this location .
+// we set limit =1 to fast access request.
+
+// Goal: Print the lat/lang for  Los Angeles
+//
+//1. Fire off a new request to the URL explored in Browser
+//2.Have the request module parse it as JSON
+//3. Print both the latitude and longitude tothe terminal
+//4.Test your work.
+
+const geocodeURL =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.ey";
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  // const latitude = response.body.features[0].center[1];
+  // const longitude=response.body.features[0].center[0];
+  // console.log(latitude,longitude);
+  if (error) {
+    console.log("Unable to connect to location services");
+  } else if (response.body.features.length == 0) {
+    console.log("Unable to find location. Try another search.");
+  } else {
+    console.log(
+      response.body.features[0].center[1],
+      response.body.features[0].center[0]
+    );
+  }
+});
+
+// Goal: Handle errors for gecoding request
+//1. setup an error handler for low level errors
+//2. Test by disabling network request and runninng the app
+//3. Setup error handling for no matching result
+//4. Test by altering the search term and running the app
+
+//callback function(core of asynchronous nodejs development)
